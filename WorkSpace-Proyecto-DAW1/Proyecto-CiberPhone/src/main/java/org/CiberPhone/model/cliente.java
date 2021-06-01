c50fc5b08b342612a22dc7477763460435f4586e
Luis Alfredo Estrada Paz <lestrada@grupochema.com>
2021-06-01 15:26:24 -0500

    1.8

--- WorkSpace-Proyecto-DAW1/Proyecto-CiberPhone/src/main/java/org/CiberPhone/model/cliente.java
+++ WorkSpace-Proyecto-DAW1/Proyecto-CiberPhone/src/main/java/org/CiberPhone/model/cliente.java
@@ -2,17 +2,61 @@ package org.CiberPhone.model;
 
 public class cliente {
 
+private String nombre;
+private String Apellido;
+private String correo;
+private String contraseña;
+private int tipo;
+private String FechaCreada;
+
+
+
+
+@Override
+public String toString() {
+	return "cliente [nombre=" + nombre + ", Apellido=" + Apellido + ", correo=" + correo + ", contraseña=" + contraseña
+			+ ", tipo=" + tipo + ", FechaCreada=" + FechaCreada + "]";
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getApellido() {
+	return Apellido;
+}
+public void setApellido(String apellido) {
+	Apellido = apellido;
+}
+public String getCorreo() {
+	return correo;
+}
+public void setCorreo(String correo) {
+	this.correo = correo;
+}
+public String getContraseña() {
+	return contraseña;
+}
+public void setContraseña(String contraseña) {
+	this.contraseña = contraseña;
+}
+public int getTipo() {
+	return tipo;
+}
+public void setTipo(int tipo) {
+	this.tipo = tipo;
+}
+public String getFechaCreada() {
+	return FechaCreada;
+}
+public void setFechaCreada(String fechaCreada) {
+	FechaCreada = fechaCreada;
+}
 	
 	
 	
 	
 	
 	
-	/*`id_Cliente` int(11) NOT NULL,
-  `nom_cli` varchar(250) NOT NULL,
-  `ape_cli` varchar(250) NOT NULL,
-  `correo_cli` varchar(300) NOT NULL,
-  `pass_cli` varchar(13) NOT NULL,
-  `id_tipousu` int(11) NOT NULL,
-  `fech_crea` timestamp NOT NULL DEFAULT current_timestamp()*/
 }
